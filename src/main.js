48fcb9cc5fbb9f43da8c0efc7a4723e8323230b8
RyckoBerserk <rycko.arcr@gmail.com>
2024-09-03 21:25:31 -0600

    agregando animación caminar

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { scaleFactor } from "./constants";
 import { k } from "./kaboomCtx";
-import { displayDialogue } from "./utils";
+import { displayDialogue, setCamScale } from "./utils";
 
 k.loadSprite("spritesheet", "./spritesheet.png", {
     sliceX: 39,
@@ -77,6 +77,11 @@ k.scene("main", async () => {
             }
         }
     }
+    setCamScale(k);
+
+    k.onResize(() => {
+        setCamScale(k);
+    });
 
     k.onUpdate(() => {
         k.camPos(player.pos.x, player.pos.y + 100);
@@ -87,6 +92,21 @@ k.scene("main", async () => {
 
         const worldMousePos = k.toWorld(k.mousePos());
         player.moveTo(worldMousePos, player.speed);
+
+        const mouseAngle = player.pos.angle(worldMousePos);
+
+        const lowerBound = 50;
+        const upperBound = 125;
+
+        if(
+            mouseAngle > lowerBound &&
+            mouseAngle < upperBound &&
+            player.curAnim() !== "walk-up"
+        ){
+            player.play("walk-up");
+            player.direction("up");
+            return;
+        }
     })
 });
 
